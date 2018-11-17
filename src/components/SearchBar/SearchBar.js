3b0ef9bed374e7b8973b6be4e91f442dfe75575d
Salma Mohammed <sm.71175@gmail.com>
2018-11-17 12:34:44 +0200

    Add render function to the SearchBar class.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,9 +9,30 @@ const sortByOptions = {
 
 class SearchBar extends React.Component {
 	renderSortByOptions() {
-		return Object.keys(sortByOptions).map(function() {
-
+		return Object.keys(sortByOptions).map(function(sortByOption) {
+			let sortByOptionValue = sortByOptions[sortByOption];
+			return `<li key={sortByOptionValue}> sortByOptionValue </li>`;
 		});
+	}
+
+	render() {
+		return(
+			<div className="SearchBar">
+			  <div className="SearchBar-sort-options">
+			    <ul>
+			      {this.renderSortByOptions}
+			    </ul>
+			  </div>
+			  <div className="SearchBar-fields">
+			    <input placeholder="Search Businesses" />
+			    <input placeholder="Where?" />
+			  </div>
+			  <div className="SearchBar-submit">
+			    <a>Let's Go</a>
+			  </div>
+			</div>
+
+			)
 	}
 }
 
