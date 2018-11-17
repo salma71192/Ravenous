6c6c44ba876415df3e2a7ef1f29878e56f676c77
Salma Mohammed <sm.71175@gmail.com>
2018-11-17 12:23:30 +0200

    Create searchBar class.

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+	'Best Match': 'best_match',
+	'Highest Rated': 'rating',
+	'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+	render() {
+		return (
+
+			)
+	}
+}
+
+export default SearchBar;
