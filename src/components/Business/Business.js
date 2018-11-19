2baf747f00cdf7713c2ef8ad5700ba2a3e9113d5
Salma Mohammed <sm.71175@gmail.com>
2018-11-19 16:50:58 +0200

    Move business object from Business.js to App.js

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,18 +2,6 @@ import React from 'react';
 import './Business.css';
 
 
-const business = {
-		  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-		  name: 'MarginOtto Pizzeria',
-		  address: '1010 Paddington Way',
-		  city: 'Flavortown',
-		  state: 'NY',
-		  zipCode: '10101',
-		  category: 'Italian',
-		  rating: 4.5,
-		  reviewCount: 90
-		}
-
 export class Business extends React.Component {
 	render() {
 		return (
